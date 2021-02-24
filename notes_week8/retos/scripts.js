458fbc3db22a9745133e31ea4d5ee571a5a64f2e
Amaru <amaruescalante@gmail.com>
2021-02-23 19:16:56 -0500

    added connection with mockapi

--- notes_week8/retos/scripts.js
+++ notes_week8/retos/scripts.js
@@ -31,7 +31,7 @@ class todoApp {
               body:JSON.stringify(objTarea), //lo tengo que convertir antes a JSON (texto)
               headers:{"Content-type":"application/json"}
             }
-            fetch(`${URL}productos`, configuracion)
+            fetch(`${URL}tareas`, configuracion)
             .then(respuesta => {
               return respuesta.json()
             })
@@ -53,16 +53,16 @@ class Interface {
             `<tr>
                 <td>
                     <input type="checkbox">
-                    <span>${tarea.nombre_tarea}</span>
+                    <span>${tarea.nombre}</span>
                 </td>
                 <td>
-                    <span class="btn btn-success btn-custom">${tarea.urgencia_tarea}</span>
+                    <span class="btn btn-success btn-custom">${tarea.urgencia}</span>
                 </td>
                 <td>
-                    <span>${tarea.prioridad_tarea}</span>
+                    <span>${tarea.prioridad}</span>
                 </td>
                 <td>
-                    <span>${tarea.fecha_tarea}</span>
+                    <span>${tarea.fecha}</span>
                 </td>
             </tr>`
             return rowTarea
@@ -85,4 +85,64 @@ let getTareas = () => {
     .catch(error => console.log(error));
 }
 
+let updateTareas = (task) => {
+    app.agregarTarea(task) 
+    .then()
+}
+
 getTareas();
+
+// MODAL ACTIONS
+
+let Modal = document.getElementById('Modal')
+const modal = new bootstrap.Modal(Modal);
+
+Modal.addEventListener('show.bs.modal', function (event) {
+    // Button that triggered the modal
+    let button = event.relatedTarget
+    let parent = button.parentNode;
+    let input = parent.childNodes[1];
+    if(input.value) {
+        console.log(input);
+
+        // If necessary, you could initiate an AJAX request here
+        // and then do the updating in a callback.
+        //
+        // Update the modal's content.
+        let modalTitle = Modal.querySelector('.modal-title')
+        // let modalBodyInput = Modal.querySelector('.modal-body input')
+
+        modalTitle.textContent = `Nueva Tarea: ${input.value}`;
+        // modalBodyInput.value = recipient
+    } else{
+        // event.preventDefault();
+        console.log("no hay valor");
+    }
+})
+
+
+
+// get input from modal form and create a new object
+
+createbutton = document.getElementById("create-task");
+
+createbutton.addEventListener('click', (event)=>{
+    console.log("sasdfasd");
+    nombre = document.getElementById("nueva-tarea");
+    let form = document.getElementById("formcreartarea")
+
+    task = new Tarea(nombre.value,
+                    form['prioridad'].value,
+                    form['urgencia'].value,
+                    form['duracion'].value);
+
+    console.log(task);
+
+    app.agregarTarea(task)
+    .then(tareaCreada => {
+        form.reset()
+        getTareas()
+        modal.hide()
+    })
+    .catch(error => console.log(error));
+})
